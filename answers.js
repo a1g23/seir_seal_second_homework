c6e5d2073b3fe24036ec4e33aaf5dad9eaf2f810
Alex Greenberg <acgreenberg2@gmail.com>
2023-11-05 10:34:08 -0500

    FizzBuzz answered

--- answers.js
+++ answers.js
@@ -15,8 +15,37 @@ for (let i = 0; i < 201; i += 2) {
 ////////////////////////////////
 // Fizz Buzz
 ////////////////////////////////
-
-
+// Write a javascript application that logs all numbers from 1 - 100.
+for (let i = 1; i < 101; i++) {
+    console.log('fizz buzz', i)
+}
+// If a number is divisible by 3 log "Fizz" instead of the number.
+for (let i = 1; i < 101; i++) {
+    const value = i
+    if(value % 3 === 0) {
+        console.log("Fizz")
+    } else {
+        console.log(i)
+    }
+}
+// If a number is divisible by 5 log "Buzz" instead of the number.
+for (let i = 1; i < 101; i++) {
+    const value = i
+    if(value % 5 === 0) {
+        console.log("Buzz")
+    } else {
+        console.log(i)
+    }
+}
+// If a number is divisible by 3 and 5 log "FizzBuzz" instead of the number.
+for (let i = 1; i < 101; i++) {
+    const value = i
+    if(value % 3 === 0 || value % 5 === 0) {
+        console.log("FizzBuzz")
+    } else {
+        console.log(i)
+    }
+}
 ////////////////////////////////
 // Wild Wild Life
 ////////////////////////////////
