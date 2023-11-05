1788fd54ce01fe8b7f9e7586d451003621d9d65f
Alex Greenberg <acgreenberg2@gmail.com>
2023-11-05 11:01:12 -0500

    Wild Wild Life answered

--- answers.js
+++ answers.js
@@ -49,7 +49,61 @@ for (let i = 1; i < 101; i++) {
 ////////////////////////////////
 // Wild Wild Life
 ////////////////////////////////
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
 
+// translating the above into a object array for easier access to keys and values
+const animals = [
+    {
+    name: "Wolfy",
+    species: "wolf",
+    age: 16,
+    hometown: "Yukon Territory"
+    },
+    {
+    name: "Sharky",
+    species: "shark",
+    age: 20,
+    hometown: "Left Coast"
+    },
+    {
+    name: "Plantee",
+    species: "plant",
+    age: 5000,
+    hometown: "Mordor"
+    },
+    {
+    name: "Porgee",
+    species: "Porg",
+    age: 186,
+    hometown: "Ahch-To"
+    },
+    {
+    name: "D'Art" ,
+    species: "Demogorgan Dog",
+    age: 2,
+    hometown: "Upside Down"
+    }
+]
+      
+// Plantee just had her birthday; change Plantee's array to reflect her being a year older.
+animals[2].age = (animals[2].age + 1)
+console.log(animals[2])
+
+//Change Wolfy's hometown from "Yukon Territory" to "Gotham City".
+animals[0].hometown = "Gotham City"
+console.log(animals[0])
+
+//Give D'Art a second hometown by adding "Hawkins"
+animals[4].hometown = animals[4].hometown + ", Hawkins"
+console.log(animals[4])
+
+//Porgee decides that Wolfy can't be named "Wolfy" anymore. Remove "Wolfy" from the wolfy array and replace it with "Gameboy"
+animals[0].name = "Gameboy"
+console.log(animals[0])
 
 ////////////////////////////////
 // Yell at the Ninja Turtles
